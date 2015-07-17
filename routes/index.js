86e5f19f8ac16fa8c2ea3614ea8d234f615e0bec
Diego Fernando Marin <dfmarin@gmail.com>
2015-07-17 11:19:48 -0500

    Modificaciones Quiz 1, módulo 6

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* Inicio */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* Créditos */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
+
+/* Preguntas & Respuestas */
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
