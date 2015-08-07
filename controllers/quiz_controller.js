747009af42c2a6f706d94cbc9f7f8115e02fe2fd
Diego Fernando Marin <dfmarin@gmail.com>
2015-08-06 19:22:10 -0500

    Despliegue de la DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,16 +3,18 @@
 // GIT: https://github.com/dfmarin/quiz
 // Autor: dfmarin@gmail.com
 
+var models = require('../models/models.js');
+
 // GET /quizes/question
 exports.question = function(req, res) {
-   models.Quiz.findAll().success( function(quiz) {
+   models.Quiz.findAll().then( function(quiz) {
       res.render('quizes/question', { pregunta: quiz[0].pregunta });
    })
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-   models.Quiz.findAll().success( function(quiz) {
+   models.Quiz.findAll().then( function(quiz) {
       if (req.query.respuesta === quiz[0].respuesta) {
          res.render('quizes/answer', { respuesta: 'Correcto' });
       } else {
@@ -20,3 +22,4 @@ exports.answer = function(req, res) {
       }
    })
 };
+
